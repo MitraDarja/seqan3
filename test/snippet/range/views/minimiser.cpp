a95dfb02fca766cab85e538e6c00e397af65a344
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2020-04-20 21:03:46 +0200

    [TEST] Add snippet

--- test/snippet/range/views/minimiser.cpp
+++ test/snippet/range/views/minimiser.cpp
@@ -1,5 +1,6 @@
 #include <seqan3/alphabet/nucleotide/dna4.hpp>
 #include <seqan3/core/debug_stream.hpp>
+#include <seqan3/range/views/complement.hpp>
 #include <seqan3/range/views/kmer_hash.hpp>
 #include <seqan3/range/views/minimiser.hpp>
 
@@ -18,4 +19,12 @@ int main()
 
     // kmer_hash with gaps, hashes: [2,7,8,14,1], minimiser: [2,1]
     seqan3::debug_stream << (text | seqan3::views::kmer_hash(0b101_shape) | seqan3::views::minimiser(4)) << '\n';
+
+    // Minimiser view with two ranges
+    auto hashes2 = text | seqan3::views::complement | std::views::reverse
+                        | seqan3::views::kmer_hash(seqan3::ungapped{3}) | std::views::reverse;
+    seqan3::debug_stream << hashes2 << '\n'; // [27,6,49,28,39]
+
+    auto minimiser2 = hashes | seqan3::views::minimiser(4, hashes2);
+    seqan3::debug_stream << minimiser2 << '\n'; // [6,6]
 }
